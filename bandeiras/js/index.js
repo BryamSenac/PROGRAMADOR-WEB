c264141840b5965ae4f837f7344203a17d934dbd
Bryam Assolini <bryam.assolini@docente.pr.senac.br>
2025-03-17 17:13:53 -0300

    att bandeiras v1.01
    Atualização de correções de lógica nos botões rodapé e do movimento do cabeçalho.

--- bandeiras/js/index.js
+++ bandeiras/js/index.js
@@ -1,3 +1,8 @@
 import { swapFlag } from "./flag_swap_controller.js";
+import { headerScrollcontroller } from "./header_scroll_controller.js";
 
 window.swapFlag = swapFlag;  // Torna a função disponível no HTML
+
+document.addEventListener("DOMContentLoaded", function () {
+    headerScrollcontroller();
+});
