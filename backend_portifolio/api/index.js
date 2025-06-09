2bc76d68460dd1799ba8c7172fa065e8814049ad
Bryam Assolini <bryam.assolini@docente.pr.senac.br>
2025-06-09 15:12:51 -0300

    ajsute

--- backend_portifolio/api/index.js
+++ backend_portifolio/api/index.js
@@ -1,18 +1,53 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+const { projetosCollection } = require('./../firebase');
 
-const projetosRoutes = require('./routers/projetos');
-const formacoesRoutes = require('./routers/formacao');
+// const projetosRoutes = require('./routers/projetos');
+// const formacoesRoutes = require('./routers/formacao');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
 
-app.use('/projetos', projetosRoutes);
-app.use('/formacoes', formacoesRoutes);
+
+app.get('/', async (req, res) => {
+    try {
+        const snapshot = await projetosCollection.get();
+        const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        res.send(data);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const docRef = await projetosCollection.add(req.body);
+        res.status(201).send({ id: docRef.id });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.put('/:id', async (req, res) => {
+    try {
+        await projetosCollection.doc(req.params.id).update(req.body);
+        res.send({ message: 'Atualizado com sucesso' });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.delete('/:id', async (req, res) => {
+    try {
+        console.log(req.params.id)
+        await projetosCollection.doc(req.params.id).delete();
+        res.send({ message: 'Deletado com sucesso' });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
 
 // app.listen(port, () => {
 //     console.log(`Servidor rodando em http://localhost:${port}`);
